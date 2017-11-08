8b8a9ff5cfe2d343d31a42409cc17e15d4176df3
Joaquín Duro Duro <joaquin.duro@gmail.com>
2017-11-08 20:50:22 +0100

    Initial Commit

---
+++ testjd/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testjd
+//
+//  Created by JOAQUIN DURO ARRIBAS on 8/11/17.
+//  Copyright © 2017 JOAQUIN DURO ARRIBAS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
